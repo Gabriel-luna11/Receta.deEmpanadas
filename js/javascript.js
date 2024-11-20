d1ce4dc548ba6b8b618620537b95d7510778690c
Gabriel-luna11 <lunakovalgabriel@gmail.com>
2024-11-19 23:44:44 -0300

    Add files via upload

---
+++ js/javascript.js
@@ -0,0 +1,17 @@
+function inclusive() {document.body.style.backgroundColor= "black"
+                      document.body.style.backgroundImage= "none";
+
+                       document.getElementById("header").style.backgroundColor = "#373e38";
+                       document.getElementById("header").style.color = "white";
+                       document.getElementById("main").style.backgroundColor = "#373e38";
+                       document.getElementById("mainn").style.backgroundColor = "#373e38";
+                       document.getElementById("p").style.backgroundColor = "#373e38";
+                       document.getElementById("p").style.color = "white";
+                       document.getElementById("section").style.backgroundColor = "#373e38"
+                       document.getElementById("sectionn").style.backgroundColor = "#373e38"
+                       document.getElementById("footer").style.backgroundColor = "#373e38"
+                      }
+
+                      function info() {
+                        alert("Diseñado por Gabriel Luna - Correo Electrónico: lunakovalgabriel@gmail.com");
+                      }
